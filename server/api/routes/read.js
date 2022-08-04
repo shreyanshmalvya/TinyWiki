69b8a2561173066f7f18e2c1bebe2a983f2e81d6
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-04 23:57:01 +0530

    added functionality to searcha and read routes

--- server/api/routes/read.js
+++ server/api/routes/read.js
@@ -1,11 +1,34 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
 const mongoose = require('mongoose');
 
 //create routes for scalabe architechure
 //GET request
-router.get('/', (req, res, next) => {
-    res.status(200).json({ message: 'Welcome to the API' });
+router.get('/:slug', (req, res, next) => {
+    const title = req.params.slug;
+    const response = async () => {
+        const result = await axios.get(`https://en.wikipedia.org/w/rest.php/v1/page/${title}/html`,
+            { 'Api-User-Agent': 'TinyWiki/1' }
+        );
+        const data = await result;
+        console.log(data);
+        return data;
+    };
+    response()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message : 'page fetched',
+                title : title,
+                html: result
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 module.exports = router;
--- server/api/routes/search.js
+++ server/api/routes/search.js
@@ -1,12 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 const mongoose = require('mongoose');
 const app = require('../../app');
 
 //create routes for scalabe architechure
-//GET request
-router.get('/', (req,res, next)=>{
-    res.status(200).json({message : 'Welcome to the API'});
+//handling get requesting by responding with data from api
+router.get('/:searchTerm', (req, res, next) => {
+    //create a function to get data from api 
+    const query = req.params.searchTerm;
+    const response = async () => {
+        const result = await axios.get(`https://en.wikipedia.org/w/rest.php/v1/search/page?q=${query}&limit=20`,
+            { 'Api-User-Agent': 'TinyWiki/1' }
+        );
+        const data = await result.data;
+        return data.pages;
+    }
+    response()
+        .then(result => {
+            console.log(result);
+            res.status(200).json({
+                message: 'similar pages fetched',
+                pages: result.map(page => {
+                    return {
+                        title: page.title,
+                        description: page.description,
+                        snippet: page.excerpt
+                    }
+                })
+            })
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        });
 });
 
 
