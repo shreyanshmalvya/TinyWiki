47e4983b27dddf53ecff3e697d5768f1cf9dd3c9
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-06 21:10:48 +0530

    added unathorized section

--- client/src/pages/admin/Admin.jsx
+++ client/src/pages/admin/Admin.jsx
@@ -1,24 +1,35 @@
 import React, { useEffect } from 'react'
 import LineGraph from '../../charts/LineGraph'
 import Navbar from '../../components/navbar/Navbar'
+import denied from '../../images/denied.gif'
+import './admin.css'
 
 const Admin = () => {
     const [loggedIn, setLoggedIn] = React.useState(false)
     useEffect(() => {
         let token = localStorage.getItem('token')
-        if (token !== 'null') {
+        console.log(token)
+        if (token !== null) {
             setLoggedIn(true)
         }
     }, [loggedIn]);
     return (
-        <>  
-            
+        <>
+
             <Navbar />
             {
                 loggedIn ?
                     <LineGraph />
                     :
-                    'NOT AUTHORIZED'
+                    <div className="notauthorizedWrapper">
+                        <div className="notAuthorized">
+                            Not Authorized
+                        </div>
+                        <div className="deniedimage">
+                            <img src={denied} alt="access denied" />
+                        </div>
+                    </div>
+
             }
         </>
     )
